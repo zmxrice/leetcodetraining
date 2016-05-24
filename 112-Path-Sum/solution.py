8d7247e0f4fb765f3bf472c4fb09fcef240ca40a
Mingxiang Zhu <zmxrice@qq.com>
2016-05-24 17:07:41 -0400

    [112. Path Sum][Accepted]committed by LeetCode Extension

--- 112-Path-Sum/solution.py
+++ 112-Path-Sum/solution.py
@@ -14,7 +14,6 @@ class Solution(object):
         """
         if not root:
             return False
-        if not root.left and not root.right:
-            if root.val == sum:
-                return True
+        if not root.left and not root.right and root.val == sum:
+            return True
         return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
