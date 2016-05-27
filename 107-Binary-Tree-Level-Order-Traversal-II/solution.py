5837b32541ccc1ecd90ba3f03621f627a6cfb2b7
Mingxiang Zhu <zmxrice@qq.com>
2016-05-27 15:14:49 -0400

    [107. Binary Tree Level Order Traversal II][Runtime Error]committed by LeetCode Extension

---
+++ 107-Binary-Tree-Level-Order-Traversal-II/solution.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        level = [root]
+        res = []
+        while level:
+            next, curVals = [], []
+            for node in level:
+                curVaks.append(node.val)
+                if node.left:
+                    next.append(node.left)
+                if node.right:
+                    next.append(node.right)
+            level = next
+            res.append(curVals)
+        return res[::-1]
