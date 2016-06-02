39c624a53f02c2688bc919c45453ee6f8a34ea64
Mingxiang Zhu <zmxrice@qq.com>
2016-06-02 16:27:26 -0400

    [322. Coin Change][Accepted]committed by LeetCode Extension

--- 322-Coin-Change/solution.py
+++ 322-Coin-Change/solution.py
@@ -5,14 +5,20 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
+        if amount == 0:
+            return 0
         dp = [0]*(amount+1)
         for i in xrange(amount+1):
             candidates = []
             for coin in coins:
-                if i >= coin:
-                    candidates.append(dp[i-coin]+1)
-            dp[i] = max(candidates)
-        if dp[-1] == 0:
-            return -1
-        else:
-            return dp[-1]
+                if i > coin:
+                    if dp[i-coin] != -1:
+                        candidates.append(dp[i-coin]+1)
+                elif i == coin:
+                     candidates.append(1)
+            if candidates:
+                dp[i] = min(candidates)
+            else:
+                dp[i]=-1
+     
+        return dp[-1]
