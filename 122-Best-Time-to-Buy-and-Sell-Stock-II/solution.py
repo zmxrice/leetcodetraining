3387f36279e248d8ca7670f589808fbee3536fc8
Mingxiang Zhu <zmxrice@qq.com>
2016-06-10 20:35:48 -0400

    [122. Best Time to Buy and Sell Stock II][Accepted]committed by LeetCode Extension

---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        for i in xrange(1, len(prices)):
+            profit += max(0, prices[i]-prices[i-1])
+        return profit
