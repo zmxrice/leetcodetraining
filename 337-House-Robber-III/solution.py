59c7298d954370f43d0187c1683022ce43335f98
Mingxiang Zhu <zmxrice@qq.com>
2016-07-13 14:34:32 -0400

    [337. House Robber III][Accepted]committed by LeetCode Extension

--- 337-House-Robber-III/solution.py
+++ 337-House-Robber-III/solution.py
@@ -6,16 +6,16 @@
 #         self.right = None
 
 class Solution(object):
-    def helper(self, root):
+    def getMax(self, root):
         if not root:
-            return 0, 0
-        left, noLeft = self.helper(root.left)
-        right, noRight = self.helper(root.right)
-        return max(left+right, root.val+noLeft+noRight), left+right
+            return 0,0
+        leftMax, noLeft = self.getMax(root.left)
+        rightMax, noRight = self.getMax(root.right)
+        return max(leftMax+rightMax, root.val+noLeft+noRight), leftMax+rightMax
         
     def rob(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        return self.helper(root)[0]
+        return self.getMax(root)[0]
