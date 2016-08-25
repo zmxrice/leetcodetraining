11d77da4ef46b5718d6dff7f49e6806ecc05159e
Mingxiang Zhu <zmxrice@qq.com>
2016-08-24 22:19:52 -0400

    [380. Insert Delete GetRandom O(1)][Accepted]committed by LeetCode Extension

---
+++ 380-Insert-Delete-GetRandom-O(1)/solution.py
@@ -0,0 +1,60 @@
+import random
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data = []
+        self.location = {}
+        
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.location:
+            return False
+        else:
+            self.data.append(val)
+            self.location[val] = len(self.data)-1
+            return True
+        
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.location:
+            last = self.data[-1]
+            if val == last:
+                self.data.pop()
+            else:
+                pos = self.location[val]
+                self.data[pos], self.data[-1] = self.data[-1], self.data[pos]
+                self.data.pop()
+                self.location[last] = pos
+            self.location.pop(val)
+            return True
+        else:
+            return False
+        
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return self.data[random.randint(0, len(self.data)-1)]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
