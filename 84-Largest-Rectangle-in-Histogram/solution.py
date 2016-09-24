7067f91e7fc88497535a6e4c6066490fb746044d
Mingxiang Zhu <zmxrice@qq.com>
2016-09-24 16:02:56 -0400

    [84. Largest Rectangle in Histogram][Accepted]committed by LeetCode Extension

--- 84-Largest-Rectangle-in-Histogram/solution.py
+++ 84-Largest-Rectangle-in-Histogram/solution.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        stack = [(0,0)]
+        if not heights:
+            return 0
+        heights = [0]+heights+[0]
         res = 0
-        for i in xrange(len(heights)):
-            while heights[i] < stack[-1][1]:
+        stack = []
+        for i, val in enumerate(heights):
+            while stack and val < stack[-1][1]:
                 cur = stack.pop()
-                res = max(res, cur[1]*(i-stack[-1][0]))
-            stack.append((i+1, heights[i]))
-        #deal with the last item
-        for i in xrange(1, len(stack)):
-            cur, pre = stack[i], stack[i-1]
-            res = max(res, cur[1]*(stack[-1][0]-pre[0]))
+                res = max(res, (i-stack[-1][0]-1)*cur[1])
+            stack.append((i, val))
         return res
